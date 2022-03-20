866226f33c20960199ecbdcd95b7c418e4aea9ed
Troy Kelly <tkelly@aftrs.edu.au>
2022-03-20 19:17:08 +1100

    Update run.sh

--- hak5c2/run.sh
+++ hak5c2/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bashio
 set -e
 
-LICENSE_KEY=$(bashio::config 'license.key')
-LOGGING=$(bashio::config 'logging')
+# LICENSE_KEY=$(bashio::config 'license.key')
+# LOGGING=$(bashio::config 'logging')
 SSLCERTFILE=$(bashio::config 'certfile')
 SSLKEYFILE=$(bashio::config 'keyfile')
 C2HOSTNAME=$(bashio::config 'hostname')
@@ -24,7 +24,7 @@ fi
 
 ## Main ##
 if bashio::config.true 'ssl'; then
-  SSLPASSWORD=$(date | md5)
+  # SSLPASSWORD=$(date | md5)
   bashio::log.info "Starting Hak5 C2 with SSL on ${C2HOSTNAME}"
   /usr/src/app/c2_community-${C2_ARCH} -db "${C2DB}"/c2.db -certFile /ssl/"$SSLCERTFILE" -keyFile /ssl/"$SSLKEYFILE" -https -hostname "${C2HOSTNAME}" -reverseProxy -reverseProxyPort 443 -listenport 8686 &
   WAIT_PIDS+=($!)
